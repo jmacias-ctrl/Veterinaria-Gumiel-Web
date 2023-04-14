90ad7c0bb3d9ea23ee407a131b5beb2e943221d1
LuisPob <111478061+LuisPob@users.noreply.github.com>
2023-04-13 21:24:53 -0400

    recuperar contraseña

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,13 +42,13 @@
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                                    <a class="nav-link" href="{{ route('login') }}">{{ __('Iniciar Sesión') }}</a>
                                 </li>
                             @endif
 
                             @if (Route::has('register'))
                                 <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Registrarse') }}</a>
                                 </li>
                             @endif
                         @else

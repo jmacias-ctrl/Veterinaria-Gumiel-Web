671efae40eb355302c8d36bebea80b8bed1257a2
nicolas-122 <73654969+nicolas-122@users.noreply.github.com>
2023-04-24 00:05:36 -0400

    add seeder marca insumos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,8 @@ class DatabaseSeeder extends Seeder
         // \App\Models\User::factory(10)->create();
         $this->call(RoleSeeder::class);
         $this->call(class: UserSeeder::class);
+        $this->call(class: MarcaInsumosSeeder::class);
+        $this->call(class: ProductosVentaSeeder::class);
         
     }
 }

d7890b4e381476749139ef1576b3eef41d52e2b5
Aracely Pérez <aperezf@ing.ucsc.cl>
2023-04-26 00:54:59 -0400

    validacion marcaproducto

--- app/Http/Controllers/MarcaproductoController.php
+++ app/Http/Controllers/MarcaproductoController.php
@@ -31,9 +31,16 @@ class MarcaproductoController extends Controller
 
         // return redirect()->route('marcaproductos.index')
         //     ->with('success', 'Marcaproducto created successfully.');
-
-        $marcaproductos = Marcaproducto::create(['nombre'=>$request->nombre]);
-        return redirect()->route('admin.marcaproductos.index');
+        $rule = [
+            'nombre'=>'required|string'
+        ];
+        $message = ['required'=>'El :attribute es obligatorio'];
+        $validator = Validator::make($request->all(),$rule,$message);
+        if($validator->passes()){
+            $marcaproductos = Marcaproducto::create(['nombre'=>$request->nombre]);
+            return redirect()->route('admin.marcaproductos.index')->with('success','La marca de insumo'.$request->nombre. 'fue agregado de manera satisfactoria');
+        }
+        return back()->withErrors($validator)->withInput();
     }
    
     public function delete(Request $request){

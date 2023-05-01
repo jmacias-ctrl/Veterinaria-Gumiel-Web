af2347f7ac0edb0481d2d55f21cea4b18cceed8e
Kevin Campos Venegas <kenderman.8@gmail.com>
2023-05-01 11:38:25 -0400

    landingpage modular

---
+++ resources/views/landing-page-agendar.blade.php
@@ -0,0 +1,18 @@
+<div class="bg-white">
+  <section class="d-flex justify-content-center align-items-center p-4">
+    <div class="container bg-darkgreen rounded-50 w-50 p-4">
+      <div class="imagen-check">
+        <img class="float-right" src="{{ asset('images/doc-check.png') }}">
+      </div>
+      <div>
+        <h2 class="text-white">Agendar horas</h2>
+        <p class="lead text-white">
+          Puedes solicitar tu hora con el veterinario o a servicios de peluquería.
+        </p>
+        <div class="">
+          <a href="#" class="btn btn-success btn-lg">Agendar hora</a>
+        </div>
+      </div>
+    </div>
+  </section>
+</div>

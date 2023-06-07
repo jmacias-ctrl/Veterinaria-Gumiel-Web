6ba0dcc69040757a5f3f067d5465734d973875da
jmacias-ctrl <69021407+jmacias-ctrl@users.noreply.github.com>
2023-06-07 03:22:38 -0400

    Fix Panel Usuario + Funcionarios Disponible

--- resources/views/calendario/verCalendario.blade.php
+++ resources/views/calendario/verCalendario.blade.php
@@ -19,6 +19,8 @@
         }
     </style>
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.4/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.4.0/css/responsive.bootstrap5.min.css">
 @endsection
 @section('content')
 
@@ -40,6 +42,7 @@
                                     <th>Turno Mañana</th>
                                     <th>Turno Tarde</th>
                                     <th>Estado</th>
+                                    <th>Accion</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -58,11 +61,14 @@
                                     <td>{{ $item->role }}</td>
                                     <td>{{ $item->morning_start }} - {{ $item->morning_end }}</td>
                                     <td>{{ $item->afternoon_start }} - {{ $item->afternoon_end }}</td>
-                                    <td> @if(Cache::has('user-is-online-' . $item->id))
-                                        <span class="badge text-bg-success">Conectado</span>
-                                    @else
-                                        <span class="badge text-bg-secondary">Desconectado</span>
-                                    @endif</td>
+                                    <td>
+                                        @if (Cache::has('user-is-online-' . $item->id))
+                                            <span class="badge text-bg-success">Conectado</span>
+                                        @else
+                                            <span class="badge text-bg-secondary">Desconectado</span>
+                                        @endif
+                                    </td>
+                                    <td><a name="" id="" class="btn btn-primary" href="#" role="button">Ver Perfil</a></td>
                                 @endforeach
                             </tbody>
                         </table>
@@ -78,12 +84,18 @@
 @endsection
 @section('js-after')
     <script type="text/javascript" src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.0/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.datatables.net/responsive/2.4.0/js/responsive.bootstrap5.min.js"></script>
     <script>
         $(document).ready(function() {
             $('#table').DataTable({
                 scrollY: '200px',
                 scrollCollapse: true,
+                responsive: true,
                 paging: false,
+                "ordering": false,
+                "info": false,
+                 "dom": 'rtip'
             });
         });
     </script>
